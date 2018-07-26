a64a87c6ede02bbc513ab03866b1be5601a196a5
Cosmin <cosmin_h6@yahoo.com>
2018-07-26 11:46:03 +0300

    GetProductsName and database connection

--- ToyStore/WebService1.asmx.cs
+++ ToyStore/WebService1.asmx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
@@ -16,11 +18,38 @@ namespace ToyStore
     // [System.Web.Script.Services.ScriptService]
     public class WebService1 : System.Web.Services.WebService
     {
+        SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-K2DSFB8\SQLEXPRESS; Initial Catalog=classicmodels;Integrated Security=True");
+        SqlCommand command;
+        SqlCommandBuilder commandBuilder;
+        SqlDataReader reader;
+        SqlDataAdapter adapter;
+        SqlCommandBuilder builder;
 
         [WebMethod]
         public string HelloWorld()
         {
             return "Hello World";
         }
+
+        [WebMethod]
+        public ArrayList GetProductsName()
+        {
+            ArrayList products = new ArrayList();
+            connection.Open();
+            command = connection.CreateCommand();
+            command.CommandText = "Select productName as Name From Products";
+            reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                products.Add(Convert.ToString(reader["Name"]));       
+            }
+
+            reader.Close();
+            connection.Close();
+            return products;
+
+        }
+
+       // [WebMethod]
     }
 }
