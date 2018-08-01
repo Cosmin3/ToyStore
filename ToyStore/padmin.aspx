3f0b91a91f8b518d2ba44749768c18ac9771986c
Ambrus Lehel <as_lehel11x@yahoo.com>
2018-08-01 12:38:20 +0300

    admin page

--- ToyStore/padmin.aspx
+++ ToyStore/padmin.aspx
@@ -12,24 +12,24 @@
             <p>
             <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
             <asp:DropDownList ID="DropDownList1" runat="server">
-                <asp:ListItem>Name</asp:ListItem>
-                <asp:ListItem>Price</asp:ListItem>
+                <asp:ListItem Value="productName">Name</asp:ListItem>
+                <asp:ListItem Value="buyPrice">Price</asp:ListItem>
                 </asp:DropDownList>
-            <asp:DropDownList ID="DropDownList2" runat="server">
+            <asp:DropDownList ID="DropDownList2" runat="server" OnSelectedIndexChanged="DropDownList2_SelectedIndexChanged">
                 <asp:ListItem Value="True">Ascendent</asp:ListItem>
                 <asp:ListItem Value="False">Descendent</asp:ListItem>
                 </asp:DropDownList>
             <asp:DropDownList ID="DropDownList3" runat="server"></asp:DropDownList>   
-            <asp:Button ID="Button3" runat="server" Text="search" />
-            <asp:Button ID="Button1" runat="server" Text="Add" />
+            <asp:Button ID="Button3" runat="server" Text="search" OnClick="Button3_Click" />
+            <asp:Button ID="Button1" runat="server" Text="Add" OnClick="Button1_Click" />
             <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Remove" />
             </p>
             <p>
             <asp:Label ID="Label1" runat="server" Text="Name"></asp:Label>
             <asp:Label ID="Label2" runat="server" Text="Price"></asp:Label>
            </p>
-       <asp:ListBox ID="ListBox1" runat="server" Height="130px" Width="286px"></asp:ListBox>
-        
+       <asp:ListBox ID="ListBox1" runat="server" Height="130px" Width="286px" OnSelectedIndexChanged="ListBox1_SelectedIndexChanged" ></asp:ListBox>
+       
     </form>
 </body>
 </html>
--- ToyStore/padmin.aspx.cs
+++ ToyStore/padmin.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,12 +10,66 @@ namespace ToyStore
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        WebService1 web = new WebService1();
         protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request["__EVENTARGUMENT"] != null && Request["__EVENTARGUMENT"] == "event 1")
+            {
+                Response.Redirect("modify.aspx");
+            }
+            ListBox1.Attributes.Add("ondblclick", ClientScript.GetPostBackEventReference(ListBox1, "event 1"));
+           
+            DropDownList3.Items.Add("-");
+            foreach (String a in web.GetProductLine())
+                DropDownList3.Items.Add(a);
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            String a =Convert.ToString(ListBox1.SelectedItem.Text);
+            bool ok = true;
+            String b = "";
+            for(int i=0;i<a.Length&& ok;i++)
+            {
+                if (a[i + 1] == '.' && a[i + 2] == '.' && a[i + 3] == '.')
+                    ok = false;
+                b = b + a[i];
+            }
+
+            web.deleteToy(b);
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("add.aspx");
+        }
+
+        protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
 
-        protected void Button2_Click(object sender, EventArgs e)
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            ListBox1.Items.Clear();
+            bool arg = true;
+            if (DropDownList2.SelectedItem.Text == "Descendent")
+                arg = false;
+            else
+                arg = true;
+            List<ArrayList> ts = new List<ArrayList>();
+            ts=web.filter(TextBox1.Text,arg, DropDownList1.SelectedValue,DropDownList3.SelectedItem.Text);
+            foreach(ArrayList prod in ts)
+            {
+                ListBox1.Items.Add(prod[0] + "........." + prod[6]);
+            }
+            DropDownList3.Items.Clear();
+            DropDownList3.Items.Add("-");
+            foreach (String a in web.GetProductLine())
+                DropDownList3.Items.Add(a);
+        }
+
+        protected void DropDownList2_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
